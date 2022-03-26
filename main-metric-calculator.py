b2d8bf2d2de0316d258bc7a42bd0881c903050bc
Joao Souza <jvictor.9@hotmail.com>
2022-03-26 15:19:54 -0300

    adds metric post to web server

--- main-metric-calculator.py
+++ main-metric-calculator.py
@@ -1,11 +1,52 @@
+import requests
 from metric_calculator import MetricCalculator
+from datetime import datetime
 
 root = './results'
 g_std = f'{root}/gold_standard'
 f_up = f'{root}/follow_up'
+exercise = 'extension'
 
 metric_calculator = MetricCalculator(g_std, f_up)
 
-result = metric_calculator.calculate_metrics('extension', '2022-03-21', 14)
-
+result = metric_calculator.calculate_metrics(exercise, '2022-03-21', 14)
 print('O resultado é:', result)
+
+# auth
+base_url = 'http://localhost:3333'
+
+auth_url = f'{base_url}/auth/signin'
+
+patient_auth = {
+  'email': 'jvictor.942@gmail.com',
+  'password': 'a'
+}
+
+auth_response = requests.post(auth_url, patient_auth)
+json = auth_response.json()
+
+token = json['token']
+userId = json['userId']
+
+print('response: ', json)
+print('token: ', token)
+
+# save metric
+save_followup_url = f'{base_url}/follow-up/save/{userId}'
+
+headers = {
+  'Authorization': f'Bearer {token}'
+}
+
+current_date = datetime.today().strftime('%Y-%m-%d')
+
+data = {
+  'exercise': exercise,
+  'date': current_date,
+  'metric': result
+}
+
+save_response = requests.post(save_followup_url, data=data, headers=headers)
+
+print('save_response status', save_response.status_code)
+print('save_response body', save_response.json())
